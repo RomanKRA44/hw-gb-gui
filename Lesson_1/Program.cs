5c4e37bb14cba3c5ca9f964c52dd2d1f914f261e
RomanKRA44 <adm-nppl@yandex.ru>
2022-03-01 20:27:27 +0300

    prog

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет {0}", Environment.UserName + " " + "сегодня " + DateTime.Now);
+        }
+    }
+}
